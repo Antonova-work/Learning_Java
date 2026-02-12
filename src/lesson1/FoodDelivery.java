2d864e6745d5f3ee8a747534611eeb58a5376781
Anastasiya <aadworkmaill@gmail.com>
2026-02-12 11:36:51 +0300

    exercise on working with data types and output

---
+++ src/lesson1/FoodDelivery.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class FoodDelivery {
+    public static void main (String[] args){
+        double deliveryPrice = 555.5;
+        int deliveryNumber = 3;
+        System.out.println("Цена доставки: " + deliveryPrice);
+        System.out.println("Номер заказа: " + deliveryNumber);
+    }
+        }
--- src/lesson1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package lesson1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("A little change");
-        System.out.println("Testing branches in Git!");
-    }
-}
