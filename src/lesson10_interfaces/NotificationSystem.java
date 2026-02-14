330ca732c9d9ecf02b829bd0f3271870c7dafb65
Anastasiya <aadworkmaill@gmail.com>
2026-02-14 13:34:35 +0300

    add exercise on working with interfaces

---
+++ src/lesson10_interfaces/NotificationSystem.java
@@ -0,0 +1,27 @@
+package lesson10_interfaces;
+
+public class NotificationSystem {
+    public static void main(String[] args) {
+        Messenger mySmsMessenger = new SmsMessenger();
+        mySmsMessenger.sendMessage("Привет, я смс!");
+
+        Messenger myEmailMessenger = new EmailMessenger();
+        myEmailMessenger.sendMessage("Привет, я имейл!");
+    }
+
+}
+
+interface Messenger{
+    void sendMessage(String text);
+}
+class SmsMessenger implements Messenger {
+    public void sendMessage(String text){
+        System.out.println("Отправлено SMS: " + text);
+    }
+}
+
+class EmailMessenger implements Messenger{
+    public void sendMessage(String text){
+        System.out.println("Отправлен Email: " + text);
+    }
+}
