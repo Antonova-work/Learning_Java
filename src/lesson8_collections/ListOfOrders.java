da87a19bff4407685b6059e6e8e47a41936c92da
Anastasiya <aadworkmaill@gmail.com>
2026-02-14 12:35:47 +0300

    update exercise on working with collections

--- src/lesson8_collections/ListOfOrders.java
+++ src/lesson8_collections/ListOfOrders.java
@@ -1,4 +1,22 @@
 package lesson8_collections;
+import java.util.ArrayList; // Нужно импортировать!
+
 
 public class ListOfOrders {
+    public static void main(String[] args) {
+
+        ArrayList<String> bascet = new ArrayList<>();
+
+        bascet.add("Молоко");
+        bascet.add("Хлеб");
+        bascet.add("Сыр");
+
+        System.out.println(bascet.size());
+
+        bascet.remove(1);
+
+        for (String item : bascet) {
+            System.out.println(item);
+        }
+    }
 }
