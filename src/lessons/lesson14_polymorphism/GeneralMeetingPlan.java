f3285a291cbd9f33794aadb54440c90a55d51bfa
Anastasiya <aadworkmaill@gmail.com>
2026-02-16 17:40:14 +0300

    add an exercise to work with polymorphism

---
+++ src/lessons/lesson14_polymorphism/GeneralMeetingPlan.java
@@ -0,0 +1,62 @@
+package lessons.lesson14_polymorphism;
+
+import java.util.ArrayList;
+
+public class GeneralMeetingPlan {
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = new ArrayList<>();
+
+        Manager newManager = new Manager("Саша", 1000, 500);
+        Courier newCourier = new Courier("Серёжа", 1500, "Bicycle");
+
+        staff.add(newManager);
+        staff.add(newCourier);
+
+        for (Employee employee : staff) {
+            employee.displayInfo();
+        }
+    }
+}
+
+class Employee {
+    String name;
+    int salary;
+
+    public Employee (String newName, int newSalary) {
+        this.name = newName;
+        this.salary = newSalary;
+    }
+
+    void displayInfo () {
+        System.out.println("Имя: " + this.name + " | " + " Зарплата: " + this.salary);
+    }
+}
+
+class Manager extends Employee {
+    int bonus;
+
+    public Manager (String name, int salary, int newBonus) {
+        super(name, salary);
+        this.bonus = newBonus;
+    }
+
+    @Override
+    void displayInfo () {
+        System.out.println("Имя: " + this.name + " | " + " Зарплата: " + this.salary + " | " + " Бонус: " + this.bonus);
+    }
+}
+
+class Courier extends Employee {
+    String vehicleType;
+
+    public Courier (String name, int salary, String newVehicleType) {
+        super(name, salary);
+        this.vehicleType = newVehicleType;
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Имя: " + this.name + " | " + " Зарплата: " + this.salary + " | " +
+                " Средство передвижения: " + this.vehicleType);
+    }
+}
